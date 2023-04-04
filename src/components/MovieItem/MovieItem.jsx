203a187d3b36b2761fd085a8c057386f32893e75
Дмитрий Ткаченко <dmitrytkachenko7397@gmail.com>
2023-04-04 10:00:16 +0200

    fix movie router string

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -3,13 +3,16 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import movieImg from 'img/movie.svg';
 
-const MovieItem = ({id, location, poster_path, title}) => {
-
+const MovieItem = ({ id, location, poster_path, title }) => {
   return (
     <li className={style.item}>
-      <Link className={style.link} to={`${id}`} state={{ from: location }}>
+      <Link
+        className={style.link}
+        to={location.pathname === `/movies` ? `${id}` : `/movies/${id}`}
+        state={{ from: location }}
+      >
         <img
-            className={style.itemImg}
+          className={style.itemImg}
           src={
             poster_path
               ? `https://image.tmdb.org/t/p/w300${poster_path}`
@@ -29,6 +32,6 @@ MovieItem.propTypes = {
   location: PropTypes.object,
   poster_path: PropTypes.string,
   title: PropTypes.string,
-}
+};
 
 export default MovieItem;
